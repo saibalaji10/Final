5cb01654b62e2df74adf72a1c8976e4db80441c6
saibalaji <mynameisbaju@gmail.com>
2018-04-28 11:58:42 +0530

    Second commit

--- final.py
+++ final.py
@@ -1,24 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import matplotlib.pyplot as plt
 
-from pandas.tools.plotting import scatter_matrix
-import matplotlib.pyplot as plt
-from sklearn import model_selection
-from sklearn import preprocessing
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import accuracy_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.ensemble import RandomForestClassifier
-from sklearn import naive_bayes
-from sklearn.svm import SVC
-from sklearn import tree
 
 aDTree = []
 aRbfSvm = []
